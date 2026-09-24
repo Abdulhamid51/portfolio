938ec7a3da9624ebecdef8003b6672f41c79ce2b
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2026-09-24 11:06:13 +0500

    some

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
 
 
+def _watch_env_file(sender, **kwargs):
+    """`.env` o'zgarsa, dev-server o'zi qayta ishga tushsin."""
+    from django.conf import settings
+
+    sender.watch_dir(settings.BASE_DIR, ".env")
+
+
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "core"
+
+    def ready(self):
+        from django.utils.autoreload import autoreload_started
+
+        autoreload_started.connect(_watch_env_file)
